3d13ed5e97eeb03570f17d7e6d52703730d659b0
Diego Alonso Corales Rivera <corales3112@hotmail.com>
2022-02-17 00:15:58 -0500

    subiendo cambios v1.1

--- controllers/productController.js
+++ controllers/productController.js
@@ -8,6 +8,40 @@ const { productModel } = require('../database/db');
 const { categorytModel } = require('../database/db');
 
 /**funcion obtener productos paginados */
+const getProductsAll = (req, res = response) => {
+  productModel.findAndCountAll({
+          attributes: ['id','name','url_image','price','discount'],
+          where: {
+            url_image: {
+              [Op.ne]: ""
+            }
+          },
+          include: {
+            model: categorytModel,
+            required: true
+          }
+  })
+  .then(data => {
+    if(data.length == 0) {
+      res.status(404).json({
+        ok: false,
+        message: 'No se encontraron registros.'
+      })
+    } else {
+      res.json({
+        ok: true,
+        data
+      })
+    }
+  })
+  .catch(err => {
+    res.status(500).send({
+      msg: 'Error en el servidor.'
+    });
+  });
+}
+
+/**funcion obtener productos paginados */
 const getProducts = (req, res = response) => {
   const desde = Number(req.query.desde || 0);
   productModel.findAndCountAll({
@@ -76,6 +110,7 @@ const getProductByCategory = (req, res = response) => {
 
 /**exportamos fuera para usarlo */
 module.exports = {
+  getProductsAll,
   getProducts,
   getProductByCategory
 }
--- routes/product.route.js
+++ routes/product.route.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
-const { getProducts,getProductByCategory } = require('../controllers/productController');
+const { getProducts,getProductByCategory,getProductsAll } = require('../controllers/productController');
 
 const router = Router();
 
 /**ruta para productos */
+router.get("/all", getProductsAll);
 router.get("/", getProducts);
 router.get("/category", getProductByCategory);
 
